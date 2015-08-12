bc43a5cbd40a04e24f5c29e54ee71cd452bc91b7
Chris Loew <cloew123@gmail.com>
2015-08-12 11:25:48 -0600

    Added Boilerplate Package code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='kao_flask_sslify',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['kao_flask_sslify'],
+     )
